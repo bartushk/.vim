6dff1cf4b68c505d05998ada4849850b9498c8aa
Kyle Bartush <kbartush7@gmail.com>
2016-11-23 10:26:37 -0500

    Added a bashrc, added a dev environment initialization script.

--- init.sh
+++ init.sh
@@ -6,8 +6,11 @@ mkdir ./bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 rm ~/.vimrc
+rm ~/.tmux.conf
+rm ~/.bashrc
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim/.tmux.conf ~/.tmux.conf
+ln -s ~/.vim/.bashrc ~/.bashrc
 vim +PluginInstall +qall
 
 cd ./bundle/tern_for_vim
@@ -18,5 +21,3 @@ make
 
 cd ../YouCompleteMe
 ./install.sh --clang-completer --racer-completer
-
-
