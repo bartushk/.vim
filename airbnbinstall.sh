2bf8181ef31cfebb3767ce1426a5858498772328
Kyle Bartush <bartushk@gmail.com>
2017-02-20 16:09:51 -0800

    Adding necessary babel-eslint install.

--- airbnbinstall.sh
+++ airbnbinstall.sh
@@ -4,3 +4,5 @@
     export PKG=eslint-config-airbnb;
     npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs sudo npm install -g "$PKG@latest"
 )
+
+sudo npm install -g babel-eslint
