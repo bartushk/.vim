84f41b21f9cdaee2ae158317e0e17c0b7da0b3e3
Kyle Bartush <kylebartush@google.com>
2017-03-02 14:09:09 -0800

    Adding an i3 config.

--- init.sh
+++ init.sh
@@ -8,9 +8,12 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm ~/.vimrc
 rm ~/.tmux.conf
 rm ~/.bashrc
+rm -r ~/.i3
+mkdir ~/.i3
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim/.tmux.conf ~/.tmux.conf
 ln -s ~/.vim/.bashrc ~/.bashrc
+ln -s ~/.vim/i3-config ~/.i3/config
 vim +PluginInstall +qall
 
 cd ./bundle/tern_for_vim
