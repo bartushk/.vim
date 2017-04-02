a68f374eccc710340c08d738a8fb97d2df08b938
Kyle Bartush <bartushk@memmi-util.localdomain>
2017-04-02 02:42:33 +0000

    Removed i3 stuff, it was buggy. Uses install.py now.

--- init.sh
+++ init.sh
@@ -8,16 +8,13 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 rm ~/.vimrc
 rm ~/.tmux.conf
 rm ~/.bashrc
-rm -r ~/.i3
-mkdir ~/.i3
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim/.tmux.conf ~/.tmux.conf
 ln -s ~/.vim/.bashrc ~/.bashrc
-ln -s ~/.vim/i3-config ~/.i3/config
 vim +PluginInstall +qall
 
 cd ./bundle/tern_for_vim
 npm install
 
 cd ../YouCompleteMe
-./install.sh --clang-completer --racer-completer --gocode-completer
+./install.py --clang-completer --racer-completer --gocode-completer
